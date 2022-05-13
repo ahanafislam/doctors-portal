b6aeda67bdf1e1314bf484fed7b2d7b4b3cac310
ahanafislam <ahanafislam@gmail.com>
2022-05-14 04:36:56 +0600

    Degian banner

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen w-full bg-hero-bg">
+            <div className="hero-content flex-col md:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='chair-pic'/>
+                <div>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary text-white font-bold">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('/src/assets/images/bg.png')",
+      }
+    },
   },
   daisyui: {
     themes: [
