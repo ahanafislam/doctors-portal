4ba4db9c6e3b39f1b6610f99cba0327f2f6f07ee
ahanafislam <ahanafislam@gmail.com>
2022-05-21 02:19:58 +0600

    Set firebase login and signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -13,7 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/appointment" element={<Appointment/>} />
+        <Route path="/appointment" element={
+          <RequireAuth>
+            <Appointment/>
+          </RequireAuth>
+        }/>
+        <Route path="/registration" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,109 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
     // Style variable
     const formControl = 'py-4 px-5 rounded-lg border';
+
+    let signInError;
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    useEffect( () =>{
+        if (user || gUser) {
+            navigate(from, { replace: true });
+        }
+    }, [user, gUser, from, navigate])
+
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+    if(error || gError){
+        signInError= <p className='text-red-500'><small>{error?.message || gError?.message }</small></p>
+    }
+
+    const onSubmit = data => {
+        signInWithEmailAndPassword(data.email, data.password);
+    }
+
     return (
         <section className='container mx-auto lg:px-10 h-screen flex justify-center items-center'>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
                     <div className="text-center text-xl">Login</div>
-                    <form className='flex flex-col gap-5'>
-                        <input type="email" name='email' placeholder='Email' className={formControl}/>
-                        <input type="password" name='password' placeholder='Enter Your Password' className={formControl}/>
+                    {signInError}
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5'>
+                        <div className="form-control w-full max-w-xs">
+                            <input
+                                type="email"
+                                placeholder="Enter email"
+                                className={formControl}
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid Email'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className={formControl}
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be 6 characters or longer'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
                         <Link to="/" className='text-xs'>Forget Password?</Link>
-                        <button className='btn btn-accent text-white'>Login</button>
+                        <button
+                            className='btn btn-accent text-white'
+                            type='submit'
+                        >Login</button>
                         <p className='text-xs text-center'>New to Doctors Portal? <Link to='/registration' className='mx-1 text-secondary'>Create new account</Link></p>
                         <div className="divider">OR</div>
-                        <button className='btn border bg-white border-accent text-accent hover:text-white'>Continue with google</button>
+                        <button
+                            className='btn border bg-white border-accent text-accent hover:text-white'
+                            onClick={() => signInWithGoogle()}
+                        >Continue with google</button>
                     </form>
                 </div>
             </div>
