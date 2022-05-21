c8138f9d685779f7d8bbd0cb9eff340482906278
ahanafislam <ahanafislam@gmail.com>
2022-05-21 17:59:29 +0600

    Create booking appointment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp';
 import RequireAuth from './Pages/Login/RequireAuth';
 import Navbar from './Pages/Shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -23,6 +25,7 @@ function App() {
         <Route path="/registration" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
+      <ToastContainer/>
     </div>
   );
 }
