e45e4ac2cb7b31039e1be5046e504deb984ba3d0
ahanafislam <ahanafislam@gmail.com>
2022-05-23 03:15:47 +0600

    Add manage doctor

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import MyAppointments from './Pages/Dashboard/MyAppointments';
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
 import AddDoctor from './Pages/Dashboard/AddDoctor';
+import ManageDoctors from './Pages/Dashboard/ManageDoctors';
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
           <Route path="users" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
           <Route path="addDoctor" element={<RequireAdmin><AddDoctor/></RequireAdmin>}></Route>
+          <Route path="manageDoctor" element={<RequireAdmin><ManageDoctors></ManageDoctors></RequireAdmin>}></Route>
         </Route>
         <Route path="/registration" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
