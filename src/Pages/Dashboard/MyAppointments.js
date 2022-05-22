0b7d921758df6424d3255e73d841a4349b561eb3
ahanafislam <ahanafislam@gmail.com>
2022-05-22 17:31:52 +0600

    Create user table

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,22 +1,39 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
 const MyAppointments = () => {
     // const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    let appointments = []
 
-    const { isLoading, data:appointments } = useQuery(['appointments', user.email], () =>
-        fetch(`http://localhost:5000/booking?patient=${user.email}`).then(res =>
-        res.json()
-        )
+    const { isLoading, data } = useQuery(['appointments', user.email], () =>
+        fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res =>{
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessToken');
+                    navigate('/');
+                }
+                return res.json()
+            })
     )
 
     if(isLoading) {
         return <Loading/>
     }
+
+    appointments = data;
 
     // useEffect(() => {
     //     if (user) {
@@ -42,6 +59,8 @@ const MyAppointments = () => {
                     </thead>
                     <tbody>
                         {
+                            appointments.length > 0
+                            ?
                             appointments?.map((a, index) =><tr key={index}>
                                 <th>{index + 1}</th>
                                 <td>{a.patientName}</td>
@@ -49,6 +68,10 @@ const MyAppointments = () => {
                                 <td>{a.slot}</td>
                                 <td>{a.treatment}</td>
                             </tr>)
+                            :
+                            <tr>
+                                <td colSpan="5" className='text-center'>Sorry Nothing to display</td>
+                            </tr>
                         }
                         
                         
